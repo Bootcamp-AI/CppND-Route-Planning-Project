876b3717e2373f1ceb44ab613f4157eaac01709f
Bootcamp-AI <bootcampai@gmail.com>
2023-08-09 12:32:57 -0500

    Edit route-model

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -3,10 +3,8 @@
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
     // Convert inputs to percentage:
-    start_x *= 0.01;
-    start_y *= 0.01;
-    end_x *= 0.01;
-    end_y *= 0.01;
+
+
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
@@ -58,8 +56,7 @@ RouteModel::Node *RoutePlanner::NextNode() {
 
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
     // Create path_found vector
-    distance = 0.0f;
-    std::vector<RouteModel::Node> path_found;
+
 
     // TODO: Implement your solution here.
 
--- src/route_planner.h
+++ src/route_planner.h
@@ -11,8 +11,8 @@ class RoutePlanner {
   public:
     RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
     // Add public variables or methods declarations here.
-    float GetDistance() const {return distance;}
-    void AStarSearch();
+
+
 
     // The following methods have been made public so we can test them individually.
     void AddNeighbors(RouteModel::Node *current_node);
@@ -22,9 +22,7 @@ class RoutePlanner {
 
   private:
     // Add private variables or methods declarations here.
-    std::vector<RouteModel::Node*> open_list;
-    RouteModel::Node *start_node;
-    RouteModel::Node *end_node;
+
 
     float distance = 0.0f;
     RouteModel &m_Model;
